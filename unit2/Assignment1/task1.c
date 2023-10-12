1be737c2d086578ec09c72b446a081828297ef71
bsmh <basmawasfy2@gmail.com>
2023-10-12 16:59:57 +0300

    two EX in homework1

--- unit2/Assignment1/task1.c
+++ unit2/Assignment1/task1.c
@@ -1,1 +1,14 @@
-//task1
+// Ex5
+// Write C Program to Find ASCII Value of a Character
+
+#include <stdio.h>
+void main(void)
+{
+	char character;
+	printf( "Enter a charcter : ");
+	fflush(stdout);
+	scanf("%c", &character);
+	printf( "ASCII value of %c = %d ", character, character);
+	
+}
+
