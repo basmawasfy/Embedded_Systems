fab8b7322ca79f6b005c5a46d42fce40b5f50881
bsmh <basmawasfy2@gmail.com>
2024-03-15 15:07:12 +0200

    first task toggle led

---
+++ EmbeddedC/toggle_led/Debug/Src/main.d
@@ -0,0 +1,2 @@
+Src/main.o: ../Src/main.c ../Src/platform_types.h
+../Src/platform_types.h:
---
+++ EmbeddedC/toggle_led/Src/platform_types.h
@@ -0,0 +1,23 @@
+#ifndef PLATFORM_TYPES_H_
+#define PLATFORM_TYPES_H_
+
+typedef unsigned char    uint8_t;
+typedef char             sint8_t;
+typedef unsigned short   uint16_t;
+typedef short            sint16_t;
+typedef unsigned int     uint32_t;
+typedef int              sint32_t;
+typedef unsigned long    uint64_t;
+typedef long             sint64_t;
+
+typedef volatile unsigned char    vuint8_t;
+typedef volatile char             vsint8_t;
+typedef volatile unsigned short   vuint16_t;
+typedef volatile short            vsint16_t;
+typedef volatile unsigned int     vuint32_t;
+typedef volatile int              vsint32_t;
+typedef volatile unsigned long    vuint64_t;
+typedef volatile long             vsint64_t;
+
+
+#endif
